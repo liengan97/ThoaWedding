0a8e0eaff48aacae122efa969180187f94c85961
it <nhuutai2698@gmail.com>
2025-05-06 01:00:28 +0700

    trigger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,5 +15,7 @@ async function bootstrap() {
   // app.setGlobalPrefix("api"); 
 
   await app.listen(process.env.PORT ?? 3000);
+  
+  console.log(`App running at: https://${process.env.VERCEL_URL}`);
 }
 bootstrap();
