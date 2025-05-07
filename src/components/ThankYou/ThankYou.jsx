2cde1bf40119dfed0f00d778eb68c5355fe2bb8d
it <nhuutai2698@gmail.com>
2025-05-07 19:52:21 +0700

    trigger

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -5,7 +5,7 @@ function ThankYou() {
     <div className="relative w-full min-h-screen tk-wrapper">
       <div className="absolute inset-0 bg-black opacity-30"></div>
       <div className="absolute inset-0 text-white">
-        <div className="flex flex-col space-y-4 pt-20 justify-start flex items-center w-screen text-center">
+        <div className="flex flex-col space-y-4 pt-30 justify-start flex items-center w-screen text-center">
           <h1 className="text-4xl md:text-6xl font-bold tk-title">Thank You!</h1>
           <p className="text-lg md:text-xl tk-desc">for being a part of our wedding</p>
         </div>
