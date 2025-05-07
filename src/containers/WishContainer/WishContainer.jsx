9adce81a316002a9f30d9de83699f74d823651f6
it <nhuutai2698@gmail.com>
2025-05-07 13:46:33 +0700

    trigger

--- src/containers/WishContainer/WishContainer.jsx
+++ src/containers/WishContainer/WishContainer.jsx
@@ -18,7 +18,7 @@ function WishContainer() {
   useEffect(() => {
     const eventSource = new EventSource(WedEnv.API_WISH_SSE_EVENTS);
     eventSource.onmessage = (event) => {
-      updateWishes(JSON.parse(event.dat));
+      updateWishes(JSON.parse(event.date));
     };
 
     return () => eventSource.close();
