1bfe2d1e5e199b34dd41aad5e3c6fef7922f804d
it <nhuutai2698@gmail.com>
2025-05-07 12:11:14 +0700

    trigger

--- src/components/MasonryWishGrid/MasonryWishGrid.jsx
+++ src/components/MasonryWishGrid/MasonryWishGrid.jsx
@@ -4,8 +4,8 @@ import { formatWishDate } from '@/utils/date.util';
 function MasonryWishGrid({ wishes = [] }) {
   return (
     <div className="columns-1 sm:columns-2 md:columns-3  gap-4 p-4">
-      {wishes.map((wish, index) => (
-        <div key={index} className="p-4 rounded-lg break-inside-avoid shadow-md border border-white text-white bg-gray-20 mb-4">
+      {wishes.map(wish => (
+        <div key={wish.id} className="p-4 rounded-lg break-inside-avoid shadow-md border border-white text-white bg-gray-20 mb-4">
           <WishCard whoSend={wish?.sender} createdAt={formatWishDate(wish?.atUtc)} message={wish?.message} />
         </div>
       ))}
