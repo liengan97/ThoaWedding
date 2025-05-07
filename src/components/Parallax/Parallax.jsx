a26361cde20cdf4565c82f43a24fbc5b5c4e8c27
it <nhuutai2698@gmail.com>
2025-05-07 17:46:18 +0700

    trigger

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -3,7 +3,8 @@ import './styles.css';
 function Parallax({ height, bgUrl, bgTop, children }) {
   return (
     <div
-      className={`h-${height > 0 ? height : 'screen'} parallax bg-[center_${bgTop > 0 ? bgTop : 50}%]`}
+      // className={`h-${height > 0 ? height : 'screen'} parallax bg-[center_${bgTop > 0 ? bgTop : 50}%]`}
+      className={`h-${height > 0 ? height : '[100dvh]'} parallax`}
       style={{
         backgroundImage: `url(${bgUrl})`
       }}
