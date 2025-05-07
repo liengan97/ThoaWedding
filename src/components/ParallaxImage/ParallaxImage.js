168ccd31f1f0023534c0409d2ecc0ca7261b15da
it <nhuutai2698@gmail.com>
2025-05-07 18:41:15 +0700

    trigger

--- src/components/ParallaxImage/ParallaxImage.js
+++ src/components/ParallaxImage/ParallaxImage.js
@@ -4,24 +4,12 @@ import { useEffect, useState } from "react";
 import img from '@/images/FHDie4iPjN.jpg'
 
 export default function ParallaxImage({ children }) {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => setScrollY(window.scrollY);
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
       <Image
         src={img}
         alt="Parallax Background"
         fill
-        style={{
-          transform: `translateY(${scrollY * 0.5}px)`, // Adjust speed factor
-          transition: "transform 0.1s ease-out"
-        }}
       />
       {children}
     </div>
