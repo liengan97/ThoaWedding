ef699786877bf65fed734936e555e3e54d764b67
it <nhuutai2698@gmail.com>
2025-05-07 11:42:55 +0700

    trigger

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -1,10 +1,9 @@
-import Overlay from '../Overlay/Overlay';
 import './styles.css'
 
 function ThankYou() {
   return (
     <div className="relative w-full h-full tk-wrapper">
-      <Overlay />
+      <div className="absolute inset-0 bg-black opacity-30"></div>
       <div className="absolute inset-0 text-white flex items-center justify-center text-center">
         <div className="flex flex-col space-y-4">
           <h1 className="text-4xl md:text-6xl font-bold tk-title">Thank You!</h1>
