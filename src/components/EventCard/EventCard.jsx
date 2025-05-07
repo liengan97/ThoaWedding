f85775235c9635505a9f89ed8eba490ee6a09c74
it <nhuutai2698@gmail.com>
2025-05-07 09:19:04 +0700

    trigger

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -5,14 +5,14 @@ import { faMap, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 function EventCard({ title, location, time, dayText, year, dayMonth, alternativeDay, phoneNumber, googleMapLocation, photoUrl }) {
   return (
-    <div className="mx-3 py-5">
+    <div className="mx-3 py-5 rounded-lg">
       <div className="relative w-full h-full border-1 border-gray-300 border-t-0 rounded-lg text-center bg-white">
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-4 py-1 font-bold text-gray-800 rounded-lg qsand">
           {title}
         </div>
 
-        <div className="w-full sm:h-130 lg:h-190 mr-5">
-          <Image alt=''  className="inset-0 w-full h-full object-cover" src={photoUrl} />
+        <div className="w-full sm:h-130 lg:h-190 mr-5 ">
+          <Image alt=''  className="inset-0 w-full h-full object-cover rounded-t-lg" src={photoUrl} />
         </div>
 
         <div className="pt-5 px-4">
