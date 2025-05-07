ca3e47152d5b1462af7ad15496524460ef135a19
it <nhuutai2698@gmail.com>
2025-05-07 18:36:09 +0700

    trigger

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Fixes flashing issue
+import ParallaxImage from "@/components/ParallaxImage/ParallaxImage";
 
 config.autoAddCss = false; // Prevents duplicate CSS injection
 
@@ -29,6 +30,7 @@ export default function Home() {
       <Parallax bgUrl={thankyouPhotoUrl.src} bgTop="10">
         <ThankYou />
       </Parallax>
+      <ParallaxImage />
     </div>
   );
 }
