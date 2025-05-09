666ef1c3443d14f8477a7cb743d527b00d9c40c5
it <nhuutai2698@gmail.com>
2025-05-09 23:05:41 +0700

    latest wish is now on top

--- src/containers/WishContainer/WishContainer.jsx
+++ src/containers/WishContainer/WishContainer.jsx
@@ -24,7 +24,15 @@ function WishContainer() {
   const updateWishes = (wish) => {
     setWishes(prevWishes => {
       const found = prevWishes.find(w => w.id == wish.id);
-      return found ? prevWishes : [wish, ...prevWishes];
+      if (found) {
+        return prevWishes;
+      }
+
+      const updatedWishes = [...prevWishes, wish].sort(
+        (a, b) => new Date(b.ts.seconds) - new Date(a.ts.seconds)
+      );
+
+      return updatedWishes;
     })
   }
 
