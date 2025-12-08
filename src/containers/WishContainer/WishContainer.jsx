4fdf265d7f956a7435619688f8161aef157a99d7
ngan nguyen <ntkngan.ana@gmail.com>
2025-12-08 15:07:14 +0700

    wishes container update

--- src/containers/WishContainer/WishContainer.jsx
+++ src/containers/WishContainer/WishContainer.jsx
@@ -12,6 +12,7 @@ import Parallax from "@/components/Parallax/Parallax";
 import Overlay from "@/components/Overlay/Overlay";
 
 import noti from "@/utils/notification.util";
+import { fzPhotograph } from "@/utils/font.util";
 import { PWishesBg } from "@/config/photo.config";
 import useEventPassed from "@/hooks/useEventPassed";
 
@@ -67,7 +68,10 @@ function WishContainer() {
     <>
       {
         <Section className="pb-12 px-4">
-          <Title center className="pb-5 pt-30 text-xl lg:text-3xl  text-[#425937] font-bold font-serif">
+          <Title
+            center
+            className="pb-5 pt-30 text-xl lg:text-3xl  text-[#425937] font-bold font-serif"
+          >
             Gửi lời chúc đến cặp đôi
           </Title>
           <LayoutCenter>
@@ -89,9 +93,11 @@ function WishContainer() {
             <Overlay type="warm" />
             <div className="md:w-3/5 mx-auto h-screen relative">
               <TextCenter className="h-[80px]">
-                <Title className="py-5 text-white">
-                  ✨ Lời chúc từ mọi người ✨
-                </Title>
+                <div
+                  className={`py-5 text-white text-3xl font-light ${fzPhotograph.className}`}
+                >
+                  ✨ Lời chúc phúc ✨
+                </div>
               </TextCenter>
               <div className="absolute top-[80px] bottom-20 left-0 right-0 overflow-y-scroll">
                 <MasonryWishGrid wishes={wishes} />
